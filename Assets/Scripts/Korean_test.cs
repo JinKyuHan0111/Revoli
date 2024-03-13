66d3475e5b62537ac3d09ad0206c7a8ea335dc5b
KIBO0630 <155514331+KIBO0630@users.noreply.github.com>
2024-03-13 14:27:55 +0900

    한글 꺠짐 오류(2트)

---
+++ Assets/Scripts/Korean_test.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Korean_test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ÇÑ±¹¾î
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
