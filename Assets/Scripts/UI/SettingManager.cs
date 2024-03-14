e7a0f373bb1f18cef44aa56aec373f36ca08c30f
KIBO0630 <155514331+KIBO0630@users.noreply.github.com>
2024-03-14 19:32:43 +0900

    필요없는거 삭제 & 설정창 추가

---
+++ Assets/Scripts/UI/SettingManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SettingManager : MySingleton<SettingManager>
+{
+    public Image SettingPanel;
+    public Image Bg_Panel;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //Debug.Log("ESC 누름");
+            OnClickSetting_On_Or_Close();
+        }
+        BgCtrl();
+    }
+    void BgCtrl()
+    {
+        Bg_Panel.gameObject.SetActive(SettingPanel.gameObject.activeSelf);
+    }
+    void OnClickSetting_On_Or_Close() //셋팅 버튼 닫기 버튼 클릭시 or Esc 버튼 누를시에 설정화면이 켜지거나 꺼진다.
+    {
+        if (SettingPanel.gameObject.activeSelf == true)
+        {
+            Time.timeScale = 1f;
+
+        }
+        else
+        {
+            Time.timeScale = 0f;
+        }
+        SettingPanel.gameObject.SetActive(!SettingPanel.gameObject.activeSelf); // 패널의 활성화 상태를 반전
+    }
+
+}
