a861a9f51bb36b7c7e42ea80e07afb9b36fc0668
KIBO0630 <155514331+KIBO0630@users.noreply.github.com>
2024-03-21 11:07:38 +0900

    유니티 기본공격 1타 2타 구현 및 오류 수정

--- Assets/Scripts/Player_Scripts/Basic/Player_Atk.cs
+++ Assets/Scripts/Player_Scripts/Basic/Player_Atk.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum HitType{ //공격 형식에 대한 Enum
+public enum HitType
+{
     BasicAttack,
     Skill_Prototype,
-    Etc //Etc 없애고 바꿔서 추가하면 됨
+    Etc
 }
+
 public class Player_Atk : MonoBehaviour
 {
     [Header("공격력")]
-    private float Atk_damage = 0f; //playerStats 에서 공격력을 불러와서 저장할 함수
+    private float Atk_damage = 0f;
     private PlayerStats playerStats;
 
     [Header("쿨타임 관련 변수")]
@@ -21,14 +23,15 @@ public class Player_Atk : MonoBehaviour
     public Transform pos;
     [SerializeField] private Vector2 CapsuleSize;
     public CapsuleDirection2D capsuleDirection = CapsuleDirection2D.Vertical;
-    float range_posX = 0f; //법위의 위치값
+    float range_posX = 0f;
     float range_posY = 0f;
 
     Animator anim;
     Rigidbody2D playerRb;
     Player_Move player;
     private bool doubleAttackAble = false;
-    int AttackCount = 0;
+
+    private float lastPressTime = 0f; // 마지막으로 버튼을 누른 시간
 
     void Start()
     {
@@ -40,37 +43,50 @@ public class Player_Atk : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q)& curTime <= 0)
+        if (Input.GetKeyDown(KeyCode.Q) && curTime <= 0 && doubleAttackAble == false)
         {
-            Attack(HitType.BasicAttack);
+            Attack();
         }
-        if (Input.GetKeyDown(KeyCode.W) & curTime <= 0)
+       
+        if (Input.GetKeyDown(KeyCode.W) && curTime <= 0)
         {
-            Attack(HitType.Skill_Prototype);
+            //Attack(HitType.Skill_Prototype);
+        }
+        if (Time.time - lastPressTime <= 2f & doubleAttackAble)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                //Debug.Log("더블어택 조건 달성");
+                DoubleAttack();
+            }
+        }
+        if (Time.time - lastPressTime >2f && Time.time - lastPressTime <2.05f)
+        {
+            Debug.Log("더블어택 시간 지나감");
+            Invoke("EndAttack", 0.03f);
+            doubleAttackAble = false;
         }
     }
+
     void HitRange_Setting(HitType hitType)
     {
-        switch(hitType) {
-            case HitType.BasicAttack: //basicAttack 의 공격법위 
-            {
+        switch (hitType)
+        {
+            case HitType.BasicAttack:
                 CapsuleSize = new Vector2(1, 1.5f);
-                    range_posX = 0.18f;
-                    range_posY = 0f;
-                HitRangeCalc(HitType.BasicAttack); 
+                range_posX = 0.18f;
+                range_posY = 0f;
                 break;
-            }
-            case HitType.Skill_Prototype: //Skill_Prototype 의 공격법위 
-            {
+            case HitType.Skill_Prototype:
                 CapsuleSize = new Vector2(2, 3f);
-                    range_posX = 0.28f;
-                    range_posY = 0.167f;
-                    HitRangeCalc(HitType.Skill_Prototype);
+                range_posX = 0.28f;
+                range_posY = 0.167f;
                 break;
-            }
         }
+        HitRangeCalc();
     }
-    void HitRangeCalc(HitType hitType) //HitRangeCalc(범위 값 x,범위 값 y)
+
+    void HitRangeCalc()
     {
         if (pos.localPosition.x > 0)
         {
@@ -81,131 +97,49 @@ public class Player_Atk : MonoBehaviour
             pos.localPosition = new Vector3(-range_posX, range_posY, pos.localPosition.z);
         }
     }
-    void Attack(HitType hitType)
+
+    void Attack()
     {
-        AttackCount++;
-        if (hitType == HitType.BasicAttack)
+        if (player.isAttacking == false)
         {
-            switch (AttackCount)
-            {
-                case 1:
-                    {
-                        player.isAttacking = true;
-
-                        HitRange_Setting(HitType.BasicAttack);
-
-                        Atk_damage = playerStats.attackDamage;
-                        //Debug.Log("Z버튼 누름 & curTime <=0 작동 완료"); //체크 완료
-                        Collider2D[] collider2Ds = Physics2D.OverlapCapsuleAll(pos.position, CapsuleSize, capsuleDirection, transform.rotation.z);
-                        foreach (Collider2D collider in collider2Ds)
-                        {
-                            Debug.Log("Attack1");
-                        }
-
-                        playerRb.velocity = Vector2.zero;
-
-                        anim.SetTrigger("doAttack");
-
-                        curTime = coolTime;
-
-                        Invoke("EndAttack", 1);
-
-                    }
-                    break;
-
-                case 2:
-
-                    Invoke("DoubleAttack", 0.9f);
-
-                    break;                   
-            }
-        }
-    else if (hitType == HitType.Skill_Prototype)
-        {
-            /*if (AttackCount == 1)
-            {
-                player.isAttacking = true;
-
-                HitRange_Setting(HitType.BasicAttack);
-
-                Atk_damage = playerStats.attackDamage;
-                //Debug.Log("Z버튼 누름 & curTime <=0 작동 완료"); //체크 완료
-                Collider2D[] collider2Ds = Physics2D.OverlapCapsuleAll(pos.position, CapsuleSize, capsuleDirection, transform.rotation.z);
-                foreach (Collider2D collider in collider2Ds)
-                {
-                    Debug.Log();
-                }
-                Debug.Log("Attack1");
-                
-                playerRb.velocity = Vector2.zero;
-                
-                anim.SetTrigger("doAttack");
-
-                curTime = coolTime;
-
-                Invoke("EndAttack", 0.5f);
-
-            }
-            if (AttackCount == 2)
-            {
-                Invoke("DoubleAttack", 0.3f);
-
-            }*/
-            //if (Input.GetKeyDown(KeyCode.W))
-            HitRange_Setting(HitType.Skill_Prototype);
-
+            player.isAttacking = true;
+            HitRange_Setting(HitType.BasicAttack);
+            player.can_Move = false;
             Atk_damage = playerStats.attackDamage;
-            //Debug.Log("Z버튼 누름 & curTime <=0 작동 완료"); //체크 완료
-            /*Collider2D[] collider2Ds = Physics2D.OverlapCapsuleAll(pos.position, CapsuleSize, capsuleDirection, transform.rotation.z);
+            Collider2D[] collider2Ds = Physics2D.OverlapCapsuleAll(pos.position, CapsuleSize, capsuleDirection, transform.rotation.z);
             foreach (Collider2D collider in collider2Ds)
             {
-                Debug.Log(collider.tag);
-            }*/
-            //animator.setTrigger("atk");
+                Debug.Log("Attack1");
+            }
+            anim.SetTrigger("doAttack");
             curTime = coolTime;
+
         }
         
-        /*else
-        {
-            curTime -= Time.deltaTime;
-        }*/
     }
-    // 아래 함수는 콜라이더의 윤곽선을 보여주기 위한 함수
-    // 사용한 이후 주석 처리 예정
-    private void OnDrawGizmos()
+    public void FirAttackUse()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(pos.position, CapsuleSize);
-        
+        lastPressTime = Time.time; // 발동 시간 기록
+        doubleAttackAble = true;
     }
 
-    void EndAttack()
+
+    public void EndAttack()
     {
+        Debug.Log("AttackEnd");
         player.isAttacking = false;
-        AttackCount = 0;
+        player.can_Move = true;
     }
 
     void DoubleAttack()
     {
         player.isAttacking = true;
-
         HitRange_Setting(HitType.BasicAttack);
-
         Atk_damage = playerStats.attackDamage;
-        /*//Debug.Log("Z버튼 누름 & curTime <=0 작동 완료"); //체크 완료
-        Collider2D[] collider2Ds = Physics2D.OverlapCapsuleAll(pos.position, CapsuleSize, capsuleDirection, transform.rotation.z);
-        foreach (Collider2D collider in collider2Ds)
-        {
-            Debug.Log(collider.tag);
-        }*/
         Debug.Log("Attack2");
-
-        playerRb.velocity = Vector2.zero;
-
+        player.can_Move = false;
         anim.SetTrigger("doAttack2");
-
         curTime = coolTime;
-
-        Invoke("EndAttack", 0.6f);
+        doubleAttackAble = false;
     }
 }
