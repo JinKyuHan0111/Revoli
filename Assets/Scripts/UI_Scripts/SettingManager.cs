c3c6c55ad2fa62b2e9868b4773442286707955af
KIBO0630 <155514331+KIBO0630@users.noreply.github.com>
2024-03-28 14:03:07 +0900

    게임 끄는 버튼 추가

--- Assets/Scripts/UI_Scripts/SettingManager.cs
+++ Assets/Scripts/UI_Scripts/SettingManager.cs
@@ -24,6 +24,7 @@ public class SettingManager : MySingleton<SettingManager>
     }
     public void OnClickSetting_On_Or_Close() //셋팅 버튼 닫기 버튼 클릭시 or Esc 버튼 누를시에 설정화면이 켜지거나 꺼진다.
     {
+        Debug.Log("OnClickSetting_On_Or_Close");
         if (SettingPanel.gameObject.activeSelf == true)
         {
             Time.timeScale = 1f;
@@ -35,5 +36,14 @@ public class SettingManager : MySingleton<SettingManager>
         }
         SettingPanel.gameObject.SetActive(!SettingPanel.gameObject.activeSelf); // 패널의 활성화 상태를 반전
     }
-
+    public void OnclickQuitButton()
+    {
+        Debug.Log("종료버튼 누름");
+        // 에디터에서는 종료할 수 없으므로 play 모드에서만 종료하도록 조건을 설정합니다.
+    #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+    #else
+                Application.Quit(); // 애플리케이션 종료
+    #endif
+    }
 }
